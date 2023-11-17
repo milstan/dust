bfc55e639effbbbd6dc82573cb35ec18c45c9895
Daphné Popin <daphne.popin@gmail.com>
2023-11-17 15:09:11 +0100

    Connector: Move models in file per connector

--- connectors/src/connectors/notion/lib/parents.ts
+++ connectors/src/connectors/notion/lib/parents.ts
@@ -8,12 +8,8 @@ import {
   getPageChildrenOf,
 } from "@connectors/connectors/notion/lib/connectors_db_helpers";
 import { updateDocumentParentsField } from "@connectors/lib/data_sources";
-import {
-  Connector,
-  ModelId,
-  NotionDatabase,
-  NotionPage,
-} from "@connectors/lib/models";
+import { Connector, ModelId } from "@connectors/lib/models";
+import { NotionDatabase, NotionPage } from "@connectors/lib/models/notion";
 
 /** Compute the parents field for a notion pageOrDb See the [Design
  * Doc](https://www.notion.so/dust-tt/Engineering-e0f834b5be5a43569baaf76e9c41adf2?p=3d26536a4e0a464eae0c3f8f27a7af97&pm=s)
